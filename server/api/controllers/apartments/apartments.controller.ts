1927a723dccae383a880db27ca238da70291f2c7
Warren <clough.warren@gmail.com>
2019-08-18 10:12:19 -0500

    added comments

--- server/api/controllers/apartments/apartments.controller.ts
+++ server/api/controllers/apartments/apartments.controller.ts
@@ -6,19 +6,26 @@ export class Controller {
     const page = +req.query.page || 1;
     const itemsPerPage = +req.query.per_page || 20;
 
-    const query: any = {};
+    let query: any = {};
 
-    if(req.query.status) {
+    if (req.query.status && req.query.status!=='Commented' ) {
       query.status = req.query.status;
     }
+    if (req.query.status && req.query.status==='Commented') {
+      query = {
+        ...query,
+        comment: {$exists: true, $ne: ''}
+      }
+    }
+    console.log(query);
 
     Apartment.find(query)
       .sort({ listedAt: -1 })
-      .skip((page-1) * itemsPerPage)
+      .skip((page - 1) * itemsPerPage)
       .limit(itemsPerPage)
       .then(apartments => {
         Apartment.estimatedDocumentCount().then(noAppartments => {
-          res.set('page-size',  Math.ceil(noAppartments >0 ? noAppartments/itemsPerPage: 0)+'');
+          res.set('page-size', Math.ceil(noAppartments > 0 ? noAppartments / itemsPerPage : 0) + '');
           res.set('Access-Control-Expose-Headers', 'page-size');
           res.json(apartments);
         })
@@ -28,7 +35,7 @@ export class Controller {
   update(req: Request, res: Response): void {
     const apartment = req.body as IApartmentModel;
 
-    Apartment.findOneAndUpdate({_id : req.params.id}, {$set: apartment},{useFindAndModify: false, new: true}).then(updatedApartment => {
+    Apartment.findOneAndUpdate({ _id: req.params.id }, { $set: apartment }, { useFindAndModify: false, new: true }).then(updatedApartment => {
       res.json(updatedApartment);
     }).catch(error => {
       res.status(500).json(error);
