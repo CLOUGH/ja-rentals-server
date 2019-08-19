c6a48ffefd5f3f05f4332bc21ad8a60c7de150c2
Warren <clough.warren@gmail.com>
2019-08-19 01:07:16 -0500

    added cron job schedule to control by env

--- server/api/jobs/scraper.ts
+++ server/api/jobs/scraper.ts
@@ -1,16 +1,24 @@
-import GleanerService  from './../services/gleaner.service';
 import cron from 'node-cron';
+import moment from 'moment';
+
+import L from '../../common/logger'
+import GleanerService  from './../services/gleaner.service';
 
 
 export default class Scraper {
+    scraperCronSchedule: string;
     constructor(){
-
+        this.scraperCronSchedule = process.env.SCRAPER_CRON_SCHEDULE;
     }
 
     init() {
-        cron.schedule('* * */6 * *', () => {
-            console.log('hourly cron job ran');
-            GleanerService.scrapeAppartments();
+        cron.schedule(this.scraperCronSchedule, () => {
+            L.info('Scraper job started at '+moment().format('MMMM Do YYYY, h:mm:ss a'));
+            GleanerService.scrapeAppartments().then(response => {
+                L.info(`Scraper job completed at ${moment().format('MMMM Do YYYY, h:mm:ss a')} and processed ${response.length} records`);
+            }).catch(error => {
+                L.info(`Scraper job failed at ${moment().format('MMMM Do YYYY, h:mm:ss a')}`);
+            });
         });
     }
 }
